e9fd2d9a929945caa0f764b4bf8a17bbb876e062
RandomiaGaming <43453931+RandomiaGaming@users.noreply.github.com>
2018-11-16 08:00:11 -0800

    Update PixelColider2D.cs

--- PixelColider2D.cs
+++ PixelColider2D.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 using UnityEditor;
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(PolygonCollider2D))]
